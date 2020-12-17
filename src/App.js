c860417f39f967c5531e4826d67ce920da13545c
amdawgs <72526095+amdawgs@users.noreply.github.com>
2020-12-16 19:37:30 -0500

    updating header

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hay!
+          Hay! It's for horses
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
       </header>
