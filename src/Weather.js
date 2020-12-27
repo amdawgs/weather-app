66cf46ecdab677461bb7115e4563cea3e9b8fa96
amdawgs <72526095+amdawgs@users.noreply.github.com>
2020-12-27 15:13:32 -0500

    Added axios and started calling API

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./weather.css";
 
 export default function Weather() {
-  return (
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [temperature, setTemperature] = useState(null);
+  let [description, setDescription] = useState(null);
+  let [humidity, setHumidity] = useState(null);
+  let [wind, setWind] = useState(null);
+  let [icon, setIcon] = useState(null);
+
+  function displayWeather(response) {
+    setTemperature(response.data.main.temp);
+    setDescription(response.data.weather[0].main);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setCity(response.data.name)
+    setIcon(
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    setLoaded(true);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form>
+      <input type="search" onChange={updateCity} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
     <div className="current-weather">
       <div className="row">
         <div className="city-wrapper">
           <div className="col-sm current-city" id="display-city">
-            Tokyo
+            {city}
           </div>
-          <div className="current-humidity">Humidity: 58%</div>
-          <div className="current-wind">Wind: 2km/h</div>
+          <div className="current-humidity">Humidity: {humidity}%</div>
+          <div className="current-wind">Wind: {wind} km/h</div>
         </div>
         <div className="col-sm current-temp">
-          <span id="current-temp">19°C</span>
+          <span id="current-temp">{Math.round(temperature)}°C</span>
           <div className="current-hi" id="current-hi">
             High 32°C
           </div>
@@ -24,14 +59,25 @@ export default function Weather() {
         <div className="col-sm current-icon-desc">
           <span id="icon">
             <img
-              src="https://openweathermap.org/img/wn/01d@2x.png"
+              src={icon}
               id="current-icon"
               alt="icon"
             />
           </span>
-          <div className="current-desc">Rain</div>
+          <div className="current-desc">{description}</div>
         </div>
       </div>
     </div>
+    </div>
   );
+  } 
+  else 
+  { 
+    let city = "Tokyo";
+    let apiKey = "7c64e1e96bf3ce9fe563b6dcc5b1e9bf";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(displayWeather);
+
+    return "Loading..."
+  }
 }
